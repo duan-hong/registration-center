8da9a82e7e3e0448de6de87eeaae8b319b503e42
zhangqc <zhangqc@asiainfo.com>
2020-01-17 10:49:07 +0800

    注册中心

---
+++ src/main/java/top/duanhong/registrationcenter/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package top.duanhong.registrationcenter.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author duanhong
+ * @description
+ * @date 2020/1/14
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
